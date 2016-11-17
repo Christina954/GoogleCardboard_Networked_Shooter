5da1be1d32b3f2566b48cab3df4a57d2fd646045
Michael Umenta <michael.umenta@vanderbilt.edu>
2016-11-17 16:13:23 -0600

    revert

---
+++ Assets/Scripts/flag.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flag : MonoBehaviour {
+
+	void OnCollisionEnter(Collision coll)
+	{
+
+		var hit = coll.gameObject;
+		var score = hit.GetComponent<Score> ();
+		if (score.hasFlag == false) {
+			score.takeFlag();
+		}
+		Destroy(gameObject);
+	}
+}
