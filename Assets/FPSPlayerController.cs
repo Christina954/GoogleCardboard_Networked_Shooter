24945b2545d77e46aefd9c18f6cae29eba199bc0
Anthony Maina <anthony.maina@savana.co.ke>
2016-11-17 13:13:08 -0600

    interim tutorial point

--- Assets/FPSPlayerController.cs
+++ Assets/FPSPlayerController.cs
@@ -3,9 +3,18 @@
 [RequireComponent(typeof(PlayerMotor))]
 public class FPSPlayerController : MonoBehaviour {
 
+	[SerializeField]
 	public float speed = 5f;
+	[SerializeField]
+	private float lookSensitivity = 3f;
 
-	private PlayerMotor motor = GetComponent<PlayerMotor>();
+
+	private PlayerMotor motor;
+
+	void Start ()
+	{
+		motor = GetComponent<PlayerMotor>();
+	}
 
 	void Update() {
 		//calculate movement velocity
@@ -19,7 +28,29 @@ public class FPSPlayerController : MonoBehaviour {
 		// final movement vector
 		Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;
 
+		//Apply movement
 		motor.Move (velocity);
+
+		//calculate rotation as a 3D vector
+		float _yRot = Input.GetAxisRaw("Mouse X");
+		Vector3 _rotation = new Vector3 (0f, _yRot, 0f) * lookSensitivity;
+
+		//Apply Rotation
+		motor.Rotate(_rotation);
+
+		//Apply rotation
+		motor.Rotate(_rotation);
+
+		//Calculate camera rotation as a 3D vector (turning around)
+		float _xRot = Input.GetAxisRaw("Mouse Y");
+
+		float _cameraRotationX = _xRot * lookSensitivity;
+
+		//Apply camera rotation
+		motor.RotateCamera(_cameraRotationX);
+
+
+
 	}
 
 
