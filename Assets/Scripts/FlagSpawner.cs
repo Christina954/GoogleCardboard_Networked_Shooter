1bf349038246fd1b54997bac28f973530493ee00
Michael Umenta <michael.umenta@vanderbilt.edu>
2016-11-17 16:05:31 -0600

    initial implementation of gun

---
+++ Assets/Scripts/FlagSpawner.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class FlagSpawner : NetworkBehaviour {
+	public GameObject flagPrefab;
+	public Vector3 spawnPosition;
+
+	public override void OnStartServer(){
+		var flag = (GameObject)Instantiate(flagPrefab, spawnPosition, Quaternion.Euler( -90.0f, 45.0f, 180.0f));
+		NetworkServer.Spawn(flag);
+	}
+
+}
