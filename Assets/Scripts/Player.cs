d228a4d19554ac98529f6242d0f1269b31c92f48
ehrlichwirklich <anissa.d.alexandercs@gmail.com>
2016-11-18 09:18:26 -0600

    added falg and respawn

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,6 +13,16 @@ public class Player : NetworkBehaviour {
 		SetDefaults ();
 	}
 
+	private NetworkStartPosition[] spawnPoints;
+
+	void Start ()
+	{
+		if (isLocalPlayer)
+		{
+			spawnPoints = FindObjectsOfType<NetworkStartPosition>();
+		}
+	}
+
 	public void SetDefaults(){
 		currentHealth = maxHealth;
 	}
@@ -21,5 +31,24 @@ public class Player : NetworkBehaviour {
 		currentHealth -= amount;
 		Debug.Log (transform.name + " now has " + currentHealth + " health.");
 
+	}
+
+	[ClientRpc]
+	void RpcRespawn()
+	{
+		if (isLocalPlayer)
+		{
+			// Set the spawn point to origin as a default value
+			Vector3 spawnPoint = Vector3.zero;
+
+			// If there is a spawn point array and the array is not empty, pick one at random
+			if (spawnPoints != null && spawnPoints.Length > 0)
+			{
+				spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
+			}
+
+			// Set the player’s position to the chosen spawn point
+			transform.position = spawnPoint;
+		}
 	}
 }
