eeacc9a3abd4dc2c81eda4e85cca126fe46fbf7a
ehrlichwirklich <anissa.d.alexandercs@gmail.com>
2016-12-04 22:05:15 -0600

    Adjusting the headtracking/weird glitch with the menu screens

---
+++ Assets/Scripts/TestTurner.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestTurner : MonoBehaviour {
+
+	//Add a script that just rotates left to right to see if the haed tracking is working.
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
