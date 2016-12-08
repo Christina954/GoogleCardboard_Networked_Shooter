bbb2ab369d9a3e92c73a67ec875490f6d8fff8fa
Michael Umenta <michael.umenta@vanderbilt.edu>
2016-12-08 17:14:35 -0600

    initial working of event trigger

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,104 @@
+﻿// Copyright 2014 Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Teleporter : MonoBehaviour, IGvrGazeResponder {
+
+
+	void Start() {
+		
+	}
+
+
+	void Update() {
+		
+	}
+
+
+	void LateUpdate() {
+		GvrViewer.Instance.UpdateState();
+		if (GvrViewer.Instance.BackButtonPressed) {
+			Application.Quit();
+		}
+	}
+
+	public void SetGazedAt(bool gazedAt) {
+		GetComponent<Renderer>().material.color = gazedAt ? Color.green : Color.red;
+		//		if (count % 2 == 0) {
+		//			paddle.transform.localScale += new Vector3 (0, .3F, 0);
+		//			count++;
+		//		} else {
+		//			paddle.transform.localScale += new Vector3 (0, -.3F, 0);
+		//			count++;
+		//		}
+
+	}
+
+
+
+	public void Reset() {
+	}
+
+	public void ToggleVRMode() {
+		GvrViewer.Instance.VRModeEnabled = !GvrViewer.Instance.VRModeEnabled;
+	}
+
+	public void ToggleDistortionCorrection() {
+		GvrViewer.Instance.DistortionCorrectionEnabled =
+			!GvrViewer.Instance.DistortionCorrectionEnabled;
+	}
+
+	#if !UNITY_HAS_GOOGLEVR || UNITY_EDITOR
+	public void ToggleDirectRender() {
+		GvrViewer.Controller.directRender = !GvrViewer.Controller.directRender;
+	}
+	#endif  //  !UNITY_HAS_GOOGLEVR || UNITY_EDITOR
+
+
+	public void restart() {
+		Application.LoadLevel(0);
+	}
+
+	public void itClicked() {
+		Debug.Log("the trigger is working!!");
+	}
+
+
+
+	#region IGvrGazeResponder implementation
+
+	/// Called when the user is looking on a GameObject with this script,
+	/// as long as it is set to an appropriate layer (see GvrGaze).
+	public void OnGazeEnter() {
+		SetGazedAt(true);
+		//Cardboard.SDK.CardboardTriggered
+	}
+
+	/// Called when the user stops looking on the GameObject, after OnGazeEnter
+	/// was already called.
+	public void OnGazeExit() {
+		SetGazedAt(false);
+	}
+
+	/// Called when the viewer's trigger is used, between OnGazeEnter and OnGazeExit.
+	public void OnGazeTrigger() {
+		//TeleportRandomly();
+
+	}
+
+	#endregion
+}
